ca83c40ab1118c2b44eb95b2fc375cee537ff404
fsantos <felipe.santos@fh.com.br>
2020-11-01 22:56:42 -0300

    commit ZERO C

---
+++ programa001.cpp
@@ -0,0 +1,11 @@
+/** / PROGRAMA 001 /**/
+#include <stdio.h>
+
+main(void){
+	float price;
+	float quantity;
+	printf ("type of gas pumped:premium\n");
+	printf ("price of premium gas per gallon:$2.75 \n");
+	price=1.30*quantity;
+	printf ("your total cost is:$ %f\n",price);
+}
